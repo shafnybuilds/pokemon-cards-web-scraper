0b68288ca0ac9628843cb45bc23b755d70e28e35
shafnybuilds <shafnymhd@proton.me>
2025-07-08 16:58:46 +0530

    setting up poke spider for scraping

---
+++ src/pokedex_scraper/items.py
@@ -0,0 +1,6 @@
+import scrapy
+
+class PokedexEntry(scrapy.Item):
+    pokemon_name = scrapy.Field()
+    url = scrapy.Field()
+    html = scrapy.Field()
---
+++ src/pokedex_scraper/spiders/pokedex_spider.py
@@ -0,0 +1,32 @@
+import logging
+import re
+
+import scrapy
+from pokedex_scraper.items import PokedexEntry
+from scrapy.http import HtmlResponse
+
+# logging for internal debug printing
+logger = logging.getLogger(__name__)
+
+
+# initialize the spider
+class PokedexSpider(scrapy.Spider):
+    name = "pokedex_spider"
+
+    start_urls = ["https://pokemondb.net/static/sitemaps/pokemondb.xml"]
+
+    custom_settings = {
+        "ITEM_PIPELINES": {"pokedex_scraper.piplines.PokedexPipeline": 300}
+    }
+
+    def parse(self, response):
+        logger.debug("Extracting URLs from sitemap:XML")
+
+        # defining a namespace mapping for XML parsing
+        ns = {"s": "http://www.sitemaps.org/schemas/sitemap/0.9"}
+        urls = response.xpath("//s:url/s:loc/text()", namespaces=ns).getall()
+
+        # for this use case, i have to scrape: https:pokemondb.net/charizard
+        for url in urls:
+            if re.match(r"^https://pokemondb\.net/pokedex/[^/]+$", url):
+                yield scrapy.Request(url=url, callback=self.parse_pokemon)
